c39f11a01e3a35b99362c21b1246bacff3fef14d
ivyli15 <li.ivy5@gmail.com>
2019-01-28 22:17:01 +1100

    made it easier for multi-user setups to use preview.sh

--- preview.sh
+++ preview.sh
@@ -1,1 +1,4 @@
-docker run -p 4000:4000 --rm -it --volume="$PWD/vendor/bundle:/usr/local/bundle" --volume="$PWD:/srv/jekyll" jekyll/jekyll jekyll serve --watch
+#/bin/bash
+
+# args: 1: UID that you want the container to run as - id -u for info
+docker run -e JEKYLL_UID=$1 -p 4000:4000 --rm -it --volume="$PWD/vendor/bundle:/usr/local/bundle" --volume="$PWD:/srv/jekyll" jekyll/jekyll jekyll serve --watch
