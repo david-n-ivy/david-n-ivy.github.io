d711aa4752bbec86668956da5e7ba0c90faf9083
David Smith <smith.d.mon@gmail.com>
2021-09-25 14:20:49 +1000

    swap preview.sh for a docker-compose configuration

--- preview.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-# args: 1: UID that you want the container to run as - id -u for info
-docker run -e JEKYLL_UID=$1 -p 4000:4000 --rm -it --volume="$PWD/vendor/bundle:/usr/local/bundle" --volume="$PWD:/srv/jekyll" jekyll/jekyll jekyll serve --watch
